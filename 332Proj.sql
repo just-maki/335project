495d92679011a1331d6bbbdd1de285cbc36a0fbc
psithepyro <103456248+psithepyro@users.noreply.github.com>
2024-05-08 15:35:30 -0700

    Update 332Proj.sql
    changed the implementation of foreign keys

--- 332Proj.sql
+++ 332Proj.sql
@@ -17,46 +17,57 @@ CREATE TABLE Professors(
 );
 
 CREATE TABLE Department(
-	dep_No VARCHAR(3) NOT NULL PRIMARY KEY,
+	dep_No VARCHAR(3) NOT NULL,
 	dep_Name VARCHAR(4),
 	dep_teleNo VARCHAR(14),
 	dep_OffLoc VARCHAR(30),
-	dep_chairp VARCHAR(9) FOREIGN KEY REFERENCES Professors(pssn)
+	dep_chairp VARCHAR(9) NOT NULL,
+	PRIMARY KEY(dep_No),
+	FOREIGN KEY(dep_chairp) REFERENCES Professors(pssn),
 );
 
 CREATE TABLE Course(
-	crse_No VARCHAR(7) NOT NULL PRIMARY KEY,
+	crse_No VARCHAR(7) NOT NULL,
 	crse_Title VARCHAR(20),
 	crse_txtBook VARCHAR(20),
 	crse_Units INT,
 	crse_preReq VARCHAR(20),
-	crse_depOffered VARCHAR(3) FOREIGN KEY REFERENCES Department(dep_no)
+	crse_depOffered VARCHAR(3),
+	PRIMARY KEY(crse_No),
+	FOREIGN KEY(crse_depOffered) REFERENCES Department(dep_no)
 );
-
 CREATE TABLE Section(
-	cors_No VARCHAR(7) NOT NULL FOREIGN KEY REFERENCES Course(crse_no),
-	sect_Prof VARCHAR(9) NOT NULL FOREIGN KEY REFERENCES Professors(pssn),
+	cors_No VARCHAR(7) NOT NULL,
+	sect_Prof VARCHAR(9) NOT NULL,
 	sect_No VARCHAR(7),
 	sect_Classrm VARCHAR(10),
 	sect_seatNo INT,
 	sect_meetDays VARCHAR(20),
 	sect_startTime VARCHAR(5),
-	sect_endTime VARCHAR(5)
+	sect_endTime VARCHAR(5),
+	FOREIGN KEY(cors_No) REFERENCES Course(crse_no),
+	FOREIGN KEY(sect_Prof) REFERENCES Professors(pssn),
 );
 
 CREATE TABLE Students(
-	stdnt_CWID VARCHAR(9) NOT NULL PRIMARY KEY,
+	stdnt_CWID VARCHAR(9) NOT NULL,
 	stdnt_FName VARCHAR(15),
 	stdnt_Lname VARCHAR(15),
 	stdnt_fullName VARCHAR(30) AS (concat_ws(' ', stdnt_Fname, stdnt_Lname)),
 	stdnt_teleNo VARCHAR(12),
-	stdnt_Major VARCHAR(15) NOT NULL FOREIGN KEY REFERENCES Department(dep_no),
-	stdnt_Minor VARCHAR(15) NOT NULL FOREIGN KEY REFERENCES Department(dep_no)
+	stdnt_Major VARCHAR(15) NOT NULL,
+	stdnt_Minor VARCHAR(15) NOT NULL,
+	PRIMARY KEY(stdnt_CWID),
+	FOREIGN KEY(stdnt_Major) REFERENCES Department(dep_no),
+	FOREIGN KEY(stdnt_Minor) REFERENCES Department(dep_no)
 );
 
 CREATE TABLE EnrollRec(
-	er_Student VARCHAR(9) NOT NULL FOREIGN KEY REFERENCES Students(stdnt_CWID),
-	er_crseNo VARCHAR(7) NOT NULL FOREIGN KEY REFERENCES Section(cors_No),
-	er_sectNo VARCHAR(7) NOT NULL FOREIGN KEY REFERENCES Section(sect_No),
-	er_Grade CHAR(2)
+	er_Student VARCHAR(9) NOT NULL,
+	er_crseNo VARCHAR(7) NOT NULL,
+	er_sectNo VARCHAR(7) NOT NULL,
+	er_Grade CHAR(2),
+	FOREIGN KEY(er_Student) REFERENCES Students(stdnt_CWID),
+	FOREIGN KEY(er_crseNo) REFERENCES Section(cors_No),
+    FOREIGN KEY(er_sectNo) REFERENCES Section(sect_No)
 );
