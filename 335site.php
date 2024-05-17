300b738dd0b102d44fea3e63108b89690903ee8b
just-maki <165954874+just-maki@users.noreply.github.com>
2024-05-16 17:03:36 -0700

    Update 335site.php

--- 335site.php
+++ 335site.php
@@ -1,17 +1,26 @@
-<htmlh>
-    <head>
-     <title>File structures and database project</title>
-    </head>
+<html>
     <body>
-        <?php echo "this is the website";
-        $hostName = 'https://ecs.fullerton.edu/~wang/cpsc33203/qsid.php';
-        $userName = 'CS332c24';
-        $password = 'UwJjjxA8';
-        $dbname = 'Mariadb';
-        $link=mysqli_connect($hostName, $userName, $password, $dbName)
-        or die("unable to connect to host $hostName")
+        <?php
+            // username and password need to be replaced by your username and password
+            // dbname is the same as your username
+            $hostName = 'mariadbl';
+            $userName = 'cs332c24';
+            $password = 'UwJjjxA8';
+            $dbName = 'cs332c24';
+
+            $link = mysqli_connect($hostName, $userName, $password, $dbName)
+            or die("Unable to connect to host $hostName");
+            
+            echo 'Connected successfully<p>';
+
+            $query = "SELECT * FROM STUDENT WHERE ssn=" . $_POST["sno"];
+            $result = $link->query($query);
+            $row=$result->fetch_assoc();
+            printf("SSN: %s<br>\n", $row["ssn"]);
+            printf("First NAME: %s<br>\n", $row["fname"]);
+            printf("Last NAME: %s<br>\n", $row["lname"]);
+            $result->free_result();
+            $link->close();
         ?>
-        <script language="PHP">
-        </script>
     </body>
 </html>
