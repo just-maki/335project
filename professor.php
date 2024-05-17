46356b3fbaadc7b1a79f56be5fbad741f1588ff5
just-maki <165954874+just-maki@users.noreply.github.com>
2024-05-17 13:52:34 -0700

    Update professor.php

--- professor.php
+++ professor.php
@@ -1,18 +1,9 @@
 <?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-    // username and password need to be replaced by your username and password
-    // dbname is the same as your username
-    $hostName = 'mariadb';
-    $userName = 'cs332c24';
-    $password = 'UwJjjxA8';
-    $dbName = 'cs332c24';
+    include("conn.php");
 
-    $mysqli = sqli_connect($hostName, $userName, $password, $dbName);
-    if (!$mysqli) {
-        die("Unable to connect to host $hostName");
-    }
-    
-    echo 'Connected successfully<p>';
     $query = "SELECT c.crse_Title AS Course_Title, 
         cs.sect_Classrm AS Classroom, 
         cs.sect_meetDays AS Meeting_Days, 
@@ -21,16 +12,18 @@
     FROM Professors p
     JOIN CrSection cs ON p.pssn = cs.sect_Prof
     JOIN Course c ON cs.cors_No = c.crse_No
-    WHERE p.pssn = sno; // Replace '123456789' with the actual SSN of the professor
-
-    $query = "SELECT * FROM STUDENT WHERE ssn=" . $_POST["sno"];
-    $result = $link->query($query);
-    $row=$result->fetch_assoc();
-    printf("SSN: %s<br>\n", $row["ssn"});
-    printf("First NAME: %s<br>\n", $row["FName"]);
-    printf("Last NAME: %s<br>\n", $row["LName"]);
-    $result->free_results();
-    $link->close();
+    WHERE p.pssn = sno; // Replace '123456789' with the actual SSN of the professor"
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("i", $pssn);
+    $stmt->execute();
+    $result_set = $stmt->get_result();
+    if ($result_set->num_rows <= 0) {
+        echo "Error: professor not found";
+    }
+    if ($result_set->num_rows > 0) {
+        $result = $result_set->fetch_all(MYSQLI_ASSOC);
+    }
+    $stmt->close();
 ?>
 
 <!DOCTYPE html>
@@ -38,17 +31,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Professor SSN</title>
+    <title>PROFESSORS CLASSES</title>
 </head>
     <body>
-        <h2>Sections for Course <?php echo $crse_number?></h2>
+        <h2> <?php echo $crse_number?></h2>
         <?php if ($result): ?>  
             <?php foreach ($result as $row): 
-                echo "Section Number: " . $row['section_number'] . "<br>";
-                echo "Classroom: " . $row['classroom'] . "<br>";
-                echo "Meeting Days: " . $row['meeting_days'] . "<br>";
-                echo "Time: " . $row['start_time'] . " - " . $row['end_time'] . "<br>";
-                echo "Number of Students Enrolled: " . $row['num_students_enrolled'] . "<br><br>";
+                echo "course title:  ". $row['crse_Title'] . <br>;
+                echo "Classroom: " . $row['sect_Classrm'] . <br>;
+                echo "Meeting Days: " . $row['sect_meetDays'] . <br>;
+                echo "Time: " . $row['sect_startTime'] . " - " . $row['sect_endTime'] . <br><br>;
             endforeach; ?>
         <?php endif; ?>
     </body>
